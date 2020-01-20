54c7b53ad35d4000c2c7a357fe7a2129d0de4131
Steven Robelo <srobelo20299@columbushs.com>
2020-01-20 14:00:28 -0500

    commit

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,5 +1,6 @@
 // Write your Pizza Builder JavaScript in this file.
-
+let price = document.getElementById('price');
+let list = document.getElementById('list');
 // Constants 
 var basePrice = 10
 var ingredients = {
@@ -69,38 +70,60 @@ function renderGreenPeppers() {
 
 function renderWhiteSauce() {
   // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
-  document.querySelectorAll('.sauce-white').forEach(function($whiteSauce){
+  document.querySelectorAll('.sauce-white').forEach(function($sauce){
     if (state.whiteSauce) {
-      $whiteSauce.classList.add('sauce-white');
+      $sauce.style.visibility = "visible";
     }
     else {
-      $whiteSauce.classList.remove('sauce-white');
-  }
-  
+      $sauce.style.visibility = "hidden";
+    }
   })
 }
 
 
 function renderGlutenFreeCrust() {
   // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
-  document.querySelectorAll('.crust-gluten-free').forEach(function($crust){
+  document.querySelectorAll('.crust').forEach(function($crust){
     if (state.glutenFreeCrust) {
-      $crust.classList.add('crust-gluten-free');
+      $crust.classList.add('crust-gluten-free')
     }
     else {
-      $crust.classList.add('crust-gluten-free');
+      $crust.classList.remove('crust-gluten-free');
     }
   })
 }
 
 function renderButtons() {
   // Iteration 3: add/remove the class "active" of each `<button class="btn">`
+  if(state.mushrooms)
+  document.querySelector(".btn-mushrooms").classList.add('active')
+  else
+  document.querySelector(".btn-mushrooms").classList.remove('active')
+  
+  if(state.greenPeppers)
+  document.querySelector(".btn-green-peppers").classList.add('active')
+  else
+  document.querySelector(".btn-green-peppers").classList.remove('active')
+  
+  if(state.pepperonni)
+  document.querySelector(".btn-pepperonni").classList.add('active')
+  else
+  document.querySelector(".btn-pepperonni").classList.remove('active')
+  
+  if(!state.glutenFreeCrust)
+  document.querySelector(".btn-crust").classList.add('active')
+  else
+  document.querySelector(".btn-crust").classList.remove('active')
+
+  if(!state.whiteSauce)
+  document.querySelector(".btn-sauce").classList.add('active')
+  else
+  document.querySelector(".btn-sauce").classList.remove('active')
 }
 
 function renderPrice() {
   // Iteration 4: change the HTML of `<aside class="panel price">`
 }
-
 
 renderEverything()
 
